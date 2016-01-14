0e2ed55d231cf3319932a46dd77678ce59dd4567
Aiden Choi <aidenchoi.ys@gmail.com>
2016-01-15 00:13:11 +0900

    기본 런처 화면 추가.

---
+++ app/src/main/java/todo/fika/fikatodo/FikaTodoApplication.java
@@ -0,0 +1,17 @@
+package todo.fika.fikatodo;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by AidenChoi on 2016. 1. 14..
+ */
+@EApplication
+public class FikaTodoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/todo/fika/fikatodo/LauncherActivity.java
@@ -0,0 +1,30 @@
+package todo.fika.fikatodo;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_launcher)
+public class LauncherActivity extends AppCompatActivity {
+
+    @AfterViews
+    void afterViews() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
